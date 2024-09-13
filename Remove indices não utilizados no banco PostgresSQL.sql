fd61b25213379e3e8c904823abd82d13d56af003
Lucas Alves Correa <40003143+lucasalv@users.noreply.github.com>
2024-09-13 14:30:46 -0300

    Update Remove indices não utilizados no banco PostgresSQL.sql

--- Remove indices não utilizados no banco PostgresSQL.sql
+++ Remove indices não utilizados no banco PostgresSQL.sql
@@ -2,19 +2,16 @@ DO $$
 DECLARE
     index_record RECORD;
     sql_state TEXT;
+    index_sql text;
 BEGIN
     FOR index_record IN
-        SELECT schemaname, relname, indexrelname
-        FROM pg_stat_all_indexes
-        where schemaname = 'public' 
-    LOOP
-        BEGIN
-            EXECUTE format('DROP INDEX IF EXISTS %I.%I;', index_record.schemaname, index_record.indexrelname);
-            RAISE NOTICE 'Índice % excluído.', index_record.indexrelname;
-        EXCEPTION
-            WHEN others THEN
-                GET STACKED DIAGNOSTICS sql_state = RETURNED_SQLSTATE;
-                RAISE NOTICE 'Não foi possível excluir o índice % (SQLSTATE: %).', index_record.indexrelname, sql_state;
-        END;
+        SELECT schemaname, relname, indexrelname,idx_scan,idx_tup_read,idx_tup_fetch  
+         FROM pg_stat_all_indexes
+	   where schemaname = 'public' 
+	     and idx_scan = 0
+	     and indexrelname like '%idx_%' 
+    loop
+        index_sql := format('DROP INDEX IF EXISTS %I.%I;', index_record.schemaname, index_record.indexrelname);
+        RAISE NOTICE '%', index_sql;
     END LOOP;
 END $$;
