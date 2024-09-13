b92e27e7af76be9f9bbaefceca296d310b1bd1f9
Lucas Alves Correa <40003143+lucasalv@users.noreply.github.com>
2024-09-13 16:32:47 -0300

    Update Criar indices automaticamente.sql

--- Criar indices automaticamente.sql
+++ Criar indices automaticamente.sql
@@ -1,29 +1,29 @@
---Este script SQL é projetado para gerar e imprimir comandos de criação de índices para uma tabela específica no PostgreSQL,
---excluindo aquelas do tipo 'character varying' e 'id', e gera todas as combinações possíveis de duas colunas.
-
 DO $$
 DECLARE
-    v_table_name text := 'convocacao_ativa'; -- Substitua 'sua_tabela' pelo nome da sua tabela
-    col record;
+    v_table_name text := 'contagem'; -- Substitua 'contagem' pelo nome da sua tabela
     columns text[] := '{}';
     index_sql text;
     col_count int;
     index_counter int := 1;
 BEGIN
-    FOR col IN
-        SELECT column_name
-        FROM information_schema.columns
-        WHERE table_name = v_table_name
-          AND data_type != 'character varying'
-          AND column_name not in ('id')
-    LOOP
-        columns := array_append(columns, col.column_name);
-    END LOOP;
+    -- Obtenção das colunas, exceto as de tipo 'character varying' e 'id'
+    SELECT array_agg(column_name::text)
+    INTO columns
+    FROM information_schema.columns
+    WHERE table_name = v_table_name
+      AND data_type != 'character varying'
+      AND column_name NOT IN ('id');
+
+    -- Obter o número de colunas
     col_count := array_length(columns, 1);
+    
+    -- Verificação se há colunas suficientes para combinar
     IF col_count IS NULL OR col_count < 2 THEN
         RAISE NOTICE 'Não há colunas suficientes para formar combinações.';
         RETURN;
     END IF;
+
+    -- Criação dos índices
     FOR i IN 1..col_count LOOP
         FOR j IN i+1..col_count LOOP
             index_sql := format('CREATE INDEX %s_idx_%s ON %s (%s, %s);', 
